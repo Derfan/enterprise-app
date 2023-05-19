a25fb7153720cf367c2f00bbe268a5650bcd8ffc
Borys Medvediev <medvediev.b@gmail.com>
2023-05-19 14:00:31 +0300

    Add active link

--- src/components/Navigation/NavItem.tsx
+++ src/components/Navigation/NavItem.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { FC } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import cns from "classnames";
 
 export const NavItem = ({
   href,
@@ -9,10 +11,24 @@ export const NavItem = ({
 }: {
   href: string;
   Icon: FC<{ className: string }>;
-}) => (
-  <li>
-    <Link href={href}>
-      <Icon className="transition-all fill-greyish-blue hover:fill-red" />
-    </Link>
-  </li>
-);
+}) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={cns("group", { "pointer-events-none": isActive })}
+      >
+        <Icon
+          className={cns(
+            "transition-all fill-greyish-blue",
+            { "group-hover:fill-red": !isActive },
+            { "fill-pure-white": isActive }
+          )}
+        />
+      </Link>
+    </li>
+  );
+};
